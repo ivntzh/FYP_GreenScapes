67de49da6606a92d75beb6f7128c3e4c7d5a83e1
LiewWeiMing <liewweiming0@gmail.com>
2025-04-19 08:08:47 +0800

    Pots and Tools Setup

---
+++ Assets/Scenes/WateringCan.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class WateringCan : MonoBehaviour
+{
+    public ParticleSystem waterStream;   // Water VFX
+    public AudioSource pourSound;        // Pouring sound source
+    public float pourThreshold = 60f;    // Z-axis tilt threshold
+
+    private bool isPouring = false;
+
+    void Update()
+    {
+        float tiltZ = transform.eulerAngles.z;
+
+        if (tiltZ > 180f)
+            tiltZ -= 360f;
+
+        if (!isPouring && Mathf.Abs(tiltZ) > pourThreshold)
+        {
+            StartPouring();
+        }
+        else if (isPouring && Mathf.Abs(tiltZ) < pourThreshold - 10f)
+        {
+            StopPouring();
+        }
+    }
+
+    void StartPouring()
+    {
+        if (waterStream != null) waterStream.Play();
+        if (pourSound != null && !pourSound.isPlaying) pourSound.Play();
+
+        isPouring = true;
+        Debug.Log("💧 Pouring started");
+    }
+
+    void StopPouring()
+    {
+        if (waterStream != null) waterStream.Stop();
+        if (pourSound != null && pourSound.isPlaying) pourSound.Stop();
+
+        isPouring = false;
+        Debug.Log("🚫 Pouring stopped");
+    }
+}
