c769747b06f3eef701aff550bb1017fd433f1c05
ivntzh <ivanwee19@gmail.com>
2025-04-27 16:35:37 +0800

    try

--- Assets/Scenes/Scripts/RecycleBin.cs
+++ Assets/Scenes/Scripts/RecycleBin.cs
@@ -4,34 +4,51 @@ using Photon.Pun;
 public class RecycleBin : MonoBehaviourPun
 {
     public TrashCategory acceptedTrashType;
-    public int           rewardAmount = 1;
-    public ShopManager   shopManager;
-    public AudioClip     correctSound;
+    public int rewardAmount = 1;
+    public ShopManager shopManager;
+    public AudioClip correctSound;
 
     private void OnTriggerEnter(Collider other)
     {
-        // MasterClient processes everything
-        if (!PhotonNetwork.IsMasterClient) return;
-
-        Debug.Log("Something entered the bin: " + other.name);
-
+        // 1) Find trash type
         var trash = other.GetComponentInParent<TrashType>();
         if (trash == null) return;
 
-        bool isCorrect = (trash.trashCategory == acceptedTrashType);
-
-        // Play sound on all clients
-        photonView.RPC(nameof(PlayRecycleSoundRPC), RpcTarget.All, isCorrect);
-
-        // Destroy networked trash if possible
-        var trashGO = trash.gameObject;
-        var pv      = trashGO.GetComponent<PhotonView>();
-        if (pv != null)
-            PhotonNetwork.Destroy(trashGO);
+        // 2) Ask the host to process this trash
+        var tv = trash.GetComponent<PhotonView>();
+        if (tv != null)
+            photonView.RPC(
+                nameof(RPC_ProcessTrash),
+                RpcTarget.MasterClient,
+                tv.ViewID
+            );
         else
-            Destroy(trashGO);
+            Debug.LogError("Trash had no PhotonView!");
+    }
 
-        // Award currency if correct
+    [PunRPC]
+    void RPC_ProcessTrash(int trashViewID, PhotonMessageInfo info)
+    {
+        // 3) Only the host actually runs this
+        if (!PhotonNetwork.IsMasterClient) return;
+
+        var tv = PhotonView.Find(trashViewID);
+        if (tv == null) return;
+        var trashGO = tv.gameObject;
+        var trash   = trashGO.GetComponent<TrashType>();
+        bool isCorrect = trash.trashCategory == acceptedTrashType;
+
+        // 4) Play the sound on everyone
+        photonView.RPC(
+            nameof(RPC_PlayBinSound),
+            RpcTarget.All,
+            isCorrect
+        );
+
+        // 5) Destroy the trash
+        PhotonNetwork.Destroy(trashGO);
+
+        // 6) Award points if correct
         if (isCorrect)
         {
             shopManager.AddCurrency(rewardAmount);
@@ -45,10 +62,10 @@ public class RecycleBin : MonoBehaviourPun
     }
 
     [PunRPC]
-    void PlayRecycleSoundRPC(bool correct)
+    void RPC_PlayBinSound(bool correct)
     {
         if (correct && correctSound != null)
             AudioSource.PlayClipAtPoint(correctSound, transform.position);
-        // else: you could play a ¡°wrong¡± clip here if desired
+        // else: play a ¡°wrong¡± clip or UI toast if you like
     }
 }
